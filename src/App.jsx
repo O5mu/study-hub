98f5722af55faa18bcf4727ddf3277f89c4ecd14
Husam Maslmani <s202182110@kfupm.edu.sa>
2025-04-12 21:21:55 +0300

    feat: add routing with Login and Dashboard pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
 import './App.css'
 import Sidebar from './Sidebar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+
 function App() {
   return (
-    <div>
-      <Sidebar />
-    </div>
-
-    
+    <BrowserRouter>
+      <div className="App">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
