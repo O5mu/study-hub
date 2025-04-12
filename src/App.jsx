b6b9f249727d6790a1b6e4285cd8da6607fa8e9e
Husam Maslmani <s202182110@kfupm.edu.sa>
2025-04-12 22:11:19 +0300

    refactor: move Sidebar to components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Sidebar from './Sidebar'
+import Sidebar from './components/Sidebar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
@@ -7,14 +7,19 @@ import Dashboard from './pages/Dashboard'
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Login />} />
+    <Route
+      path="/dashboard"
+      element={
+        <>
+          <Sidebar />
+          <Dashboard />
+        </>
+      }
+    />
+  </Routes>
+</BrowserRouter>
   )
 }
 
