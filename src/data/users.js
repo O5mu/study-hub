d6a3daeb033977594ac567ee0172ccd430a95389
Husam Maslmani <s202182110@kfupm.edu.sa>
2025-04-14 10:22:50 +0300

    feat: styled login page and added login logic

---
+++ src/data/users.js
@@ -0,0 +1,6 @@
+export const users = [
+    { email: 'student@example.com', password: '1234', role: 'student' },
+    { email: 'moderator@example.com', password: '1234', role: 'moderator' },
+    { email: 'admin@example.com', password: '1234', role: 'admin' },
+  ];
+  
