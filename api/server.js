b495e1b76c47990d63b81814d1245e39a944e8b6
Aziz <s202253200@kfupm.edu.sa>
2025-05-03 23:22:20 +0300

    Final Merge Test

--- api/server.js
+++ api/server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('DB Connected'))
   .catch(err => console.error('DB FAILED TO CONNECT!:', err));
